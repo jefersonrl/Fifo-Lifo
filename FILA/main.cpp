83e898ce4556cdd6ceab1d01b26035f315d01e36
Jeferson Roberto de Lima <jefersonrl@icloud.com>
2018-03-07 22:41:14 -0300

    Descrição do projeto

--- FILA/main.cpp
+++ FILA/main.cpp
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Comando utilizado para manipulação de conceito através de apelido.
+
+typedef struct ElementoLista { 
+char *dado; 
+struct ElementoLista *"seguinte"; 
+}elemento; 
+
 //Projeto para demonstração do conceito de FILA com os algoritmos de FIFO (First int First Out) e LIFO (Last in First Out)
 int main(void)
 {
