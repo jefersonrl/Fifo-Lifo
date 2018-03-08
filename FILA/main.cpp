5e47dcadab45129f5a60a27501aef90f1f632b83
Jeferson Roberto de Lima <jefersonrl@icloud.com>
2018-03-07 22:37:40 -0300

    Descrição do projeto

--- FILA/main.cpp
+++ FILA/main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Projeto para demonstração do conceito de FILA com os algoritmos de FIFO (First int First Out) e LIFO (Last in First Out)
 int main(void)
 {
 
